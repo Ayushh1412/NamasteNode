d519bed83c8a33488243836ad11e3fdb1b5bf332
Ayush Kumar <ayushkumar200264@gmail.com>
2024-10-17 14:18:10 +0530

    custom Auth using middlewares,try and catch ,wild card error handling,routes,params and query

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const {connectDB}= require('./config/database');
 const express = require('express');
 const app = express();
 const User =require('./models/user')
+const {adminAuth,userAuth} = require('./middlewares/auth')
 
 
 
@@ -14,27 +15,58 @@ connectDB().then(()=>{
     console.log("error connecting to database");
 });
 
+app.use(express.json());
 app.post("/signup",async(req,res)=>{
-   
-    const user = new User({
-        firstName:"Ayush",
-        secondName:"Kumar",
-        email:"ayush@yadav.com",
-        age:"20"
-    });
-
-   await user.save();
-    res.send("user added successfully")
+    const user = new User(req.body);
+    try{ 
+     await user.save();
+     res.send("user added successfully")
+    }catch(err){
+    res.status(400).send("Error saving the user"+ err.message);
+    }
+  
 
 });
 
-
-app.use("/admin/getUserData",(err,req,res,next)=>{
-    next();
-    res.send("sent all user data")
+app.get("/user/:token/:age",userAuth,async(req,res)=>{
+    
+    try{
+    // const userdata =  await User.find({firstName:'ishita'}).collation({ locale: 'en', strength: 2 });
+    // const userdata =  await User.find({firstName:/ishita/i});
+    const userdata =  await User.find({firstName:req.query.name}).collation({ locale: 'en', strength: 2 });
+    if(userdata.length===0)
+    {
+        res.status(400).send("sorry ,No users found");
+    }
+    else {
+        res.send(userdata);
+    }
+    }
+catch(err){
+    res.status(400).send("Error getting the users data   \n"+ err.message);
+}
+    
 });
 
+// app.use("/admin",(req,res,next)=>{
+// try{
+    
+//     throw new Error("dummy error");
+//     next();
+// } 
+// catch{
+//     res.status(500).send("something went wrong (message by try catch error handling) ")
 
+// }
+// });
+app.use("/admin",adminAuth);
 app.use("/admin",(req,res)=>{
-    res.send("hello admin")
+    // throw new Error("dummy error");
+    res.send("hello adminhvh")
+});
+
+app.use("/",(err,req,res,next)=>{
+if(err){
+    res.status(500).send("something went wrong (message by wildcard error handling) ")
+}
 });
---
+++ src/middlewares/auth.js
@@ -0,0 +1,31 @@
+ const adminAuth = (req,res,next)=>{
+    const token = "xyz";
+    const isAdminAuthorized = token==="xyz";
+    if(!isAdminAuthorized){
+        res.status(400).send("sorry you don't have admin authorization ")
+    }
+    else{
+        next();
+    }
+}
+
+const userAuth = (req,res,next)=>{
+    console.log("params are")
+    console.log(req.params);
+    console.log("queries are")
+    console.log(req.query);
+
+    const token = req.params.token;
+    const isUserAuthorized = token==="Ayush@123";
+    if(!isUserAuthorized){
+        res.status(400).send("sorry you are not a authorized user ")
+    }
+    else{
+        next();
+    }
+}
+
+module.exports={
+    adminAuth,
+    userAuth,
+}
